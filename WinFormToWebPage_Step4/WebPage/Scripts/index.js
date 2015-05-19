973b71262744e8376b7aa62d7f8eeb3f5ba5da37
chenjerfong <chenjerfong@gmail.com>
2015-05-19 14:36:34 +0800

    Revert "Revert "第一次簽入""
    This reverts commit 1b33451770f02237ebd7e8dbcf2ba528bf3fafd1.

---
+++ WinFormToWebPage_Step4/WebPage/Scripts/index.js
@@ -0,0 +1,53 @@
+﻿(function () { // Angular encourages module pattern, good!
+
+    var query = function () {
+        this.connectionId = "";
+        this.path = "C:\\";
+    }
+
+    var app = angular.module('myApp', []);
+    app.controller('myCtrl', ['$http', '$scope', function ($http, $scope) {
+
+        var hub = $.connection.myHub; // create a proxy to signalr hub on web server
+
+        hub.client.hello = function () {
+            alert('hello');
+        };
+
+        hub.client.ReportStep = function (message) {
+        };
+
+        hub.client.ReportResult = function (path, fileCount, totalSize) {
+        };
+
+        $scope.queryInfo = new query();
+
+        $.connection.hub.start().done(function () {
+            hub.server.register().done(function (e) {
+                $scope.queryInfo.connectionId = $.connection.hub.id;
+                if (e != "") {
+                    alert(e);
+                }
+            });
+        });
+
+        $scope.start = function () {
+            var url = "http://localhost:36344/StartRun";
+            $http.post(url, $scope.queryInfo)
+                .success(function (data, status) {
+                })
+                .error(function (data, status) {
+                });
+        };
+        $scope.stop = function () {
+            var url = "http://localhost:36344/StopRun";
+            $http.post(url, $scope.queryInfo)
+                .success(function (data, status) {
+                })
+                .error(function (data, status) {
+                });
+        };
+
+    }]);
+
+})();
