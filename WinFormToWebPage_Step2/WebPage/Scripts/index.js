12e808d3c1eab3886f687cdcde2128b13bdcc352
chenjerfong <chenjerfong@gmail.com>
2015-05-19 13:57:08 +0800

    第一次簽入

---
+++ WinFormToWebPage_Step2/WebPage/Scripts/index.js
@@ -0,0 +1,19 @@
+﻿(function () { // Angular encourages module pattern, good!
+
+    var app = angular.module('myApp', []);
+    app.controller('myCtrl', ['$http', '$scope', function ($http, $scope) {
+
+        var hub = $.connection.myHub; // create a proxy to signalr hub on web server
+
+        hub.client.hello = function () {
+            alert('hello');
+        };
+
+        $.connection.hub.start().done(function () {
+            hub.server.hello().done(function (e) {
+            });
+        });
+
+    }]);
+
+})();
